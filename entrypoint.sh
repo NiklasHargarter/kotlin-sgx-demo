283a186275d3754dff16379d4961a7dde42ae286
Niklas Hargarter <niklashargarter@gmail.com>
2023-07-12 17:03:47 +0200

    Add content do demo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
-echo "read plaintext file without Gramine-SGX in Docker:"
-java -jar /app/enclave.jar /plaintext/demo-file.txt
 
-echo "read encrypted file without Gramine-SGX in Docker:"
+echo "run Kotlin application with encrypted file without Gramine-SGX in Docker:"
 java -jar /app/enclave.jar /encrypted/demo-file-enc.txt
 
-"read encrypted file with Gramine-SGX "
+echo "/nrun Kotlin application with encrypted file with Gramine-SGX:"
 gramine-sgx-get-token --output kotlin.token --sig kotlin.sig
 gramine-sgx kotlin
+
+echo "/n decrypt demo-file with same key provided to the enclave"
+gramine-sgx-pf-crypt decrypt -w /files/wrap_key -i /encrypted/demo-file-enc.txt -o /plaintext/demo-file-dec.txt
+
+echo "/n run Kotlin application with decrypted file without Gramine-SGX"
+java -jar /app/enclave.jar /plaintext/demo-file-dec.txt
