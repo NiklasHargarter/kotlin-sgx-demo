6fb928374976d9e01fd1b704618ef5624e8a2716
Niklas Hargarter <niklashargarter@gmail.com>
2023-07-12 16:36:12 +0200

    Add java calls to demo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
+echo "read plaintext file without Gramine-SGX in Docker:"
+java -jar /plaintext/demo-file.txt
+
+echo "read encrypted file without Gramine-SGX in Docker:"
+java -jar /encrypted/demo-file-enc.txt
+
+"read encrypted file with Gramine-SGX "
 gramine-sgx-get-token --output kotlin.token --sig kotlin.sig
 gramine-sgx kotlin
