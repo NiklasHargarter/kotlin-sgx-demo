7bbdd84ad79a8b7893cbb0ce546293c60ce21aa0
Niklas Hargarter <niklashargarter@gmail.com>
2023-07-12 16:44:53 +0200

    remove type

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ echo "read plaintext file without Gramine-SGX in Docker:"
 java -jar /app/enclave.jar /plaintext/demo-file.txt
 
 echo "read encrypted file without Gramine-SGX in Docker:"
-java -jar /app/enclave /encrypted/demo-file-enc.txt
+java -jar /app/enclave.jar /encrypted/demo-file-enc.txt
 
 "read encrypted file with Gramine-SGX "
 gramine-sgx-get-token --output kotlin.token --sig kotlin.sig
