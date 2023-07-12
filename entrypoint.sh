89318ddbb4b1054ac6465030c4244c31dd88473b
Niklas Hargarter <niklashargarter@gmail.com>
2023-07-12 17:10:02 +0200

    Clean MainKt

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,12 @@
 echo "run Kotlin application with encrypted file without Gramine-SGX in Docker:"
 java -jar /app/enclave.jar /encrypted/demo-file-enc.txt
 
-echo "/nrun Kotlin application with encrypted file with Gramine-SGX:"
+echo "\nrun Kotlin application with encrypted file with Gramine-SGX:"
 gramine-sgx-get-token --output kotlin.token --sig kotlin.sig
 gramine-sgx kotlin
 
-echo "/n decrypt demo-file with same key provided to the enclave"
+echo "\ndecrypt demo-file with same key provided to the enclave"
 gramine-sgx-pf-crypt decrypt -w /files/wrap_key -i /encrypted/demo-file-enc.txt -o /plaintext/demo-file-dec.txt
 
-echo "/n run Kotlin application with decrypted file without Gramine-SGX"
+echo "\nrun Kotlin application with decrypted file without Gramine-SGX"
 java -jar /app/enclave.jar /plaintext/demo-file-dec.txt
