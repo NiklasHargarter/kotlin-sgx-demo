d5226ebd6679ba12e7612651fe808f77039c0f7a
Niklas Hargarter <niklashargarter@gmail.com>
2023-07-13 12:30:28 +0200

    Format entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,27 @@
 #!/bin/sh
-
+echo "\n###################################################################################"
+echo "direct run with encrypted file"
+echo "\n###################################################################################"
 echo "run Kotlin application with encrypted file without Gramine-SGX in Docker:"
 java -jar /app/enclave.jar /encrypted/demo-file-enc.txt
 
+echo "\n###################################################################################"
+echo "Gramin-SGX run"
+echo "\n###################################################################################"
 echo "\nrun Kotlin application with encrypted file with Gramine-SGX:"
 gramine-sgx-get-token --output kotlin.token --sig kotlin.sig
 gramine-sgx kotlin
 
+echo "\n###################################################################################"
+echo "direct run with decrypted file"
+echo "\n###################################################################################"
 echo "\ndecrypt demo-file with same key provided to the enclave"
 gramine-sgx-pf-crypt decrypt -w /files/wrap_key -i /encrypted/demo-file-enc.txt -o /plaintext/demo-file-dec.txt
 
 echo "\nrun Kotlin application with decrypted file without Gramine-SGX"
 java -jar /app/enclave.jar /plaintext/demo-file-dec.txt
 
+echo "\n###################################################################################"
+echo "conclusion"
+echo "\n###################################################################################"
 echo "\nThe Gramine-SGX run and direct Run with the decrypted file both achieve the same output. \nBut the in the Gramine-SGX run the file is never stored in a decrypted state on the system."
