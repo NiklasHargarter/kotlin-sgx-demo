26c634fa8765513f8f92e43822e764039f4ca3fd
Niklas Hargarter <niklashargarter@gmail.com>
2023-07-13 12:55:52 +0000

    text changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,27 +1,33 @@
 #!/bin/sh
 echo "\n###################################################################################"
 echo "direct run with encrypted file"
-echo "\###################################################################################"
+echo "###################################################################################"
 echo "run Kotlin application with encrypted file without Gramine-SGX in Docker:"
+echo "#"
 java -jar /app/enclave.jar /encrypted/demo-file-enc.txt
+echo "#"
 
 echo "\n###################################################################################"
 echo "Gramine-SGX run"
-echo "\###################################################################################"
+echo "###################################################################################"
 echo "\nrun Kotlin application with encrypted file with Gramine-SGX:"
 gramine-sgx-get-token --output kotlin.token --sig kotlin.sig
+echo "#"
 gramine-sgx kotlin
+echo "#"
 
 echo "\n###################################################################################"
 echo "direct run with decrypted file"
-echo "\n###################################################################################"
+echo "###################################################################################"
 echo "\decrypt demo-file with same key provided to the enclave"
 gramine-sgx-pf-crypt decrypt -w /files/wrap_key -i /encrypted/demo-file-enc.txt -o /plaintext/demo-file-dec.txt
 
 echo "\nrun Kotlin application with decrypted file without Gramine-SGX"
+echo "#"
 java -jar /app/enclave.jar /plaintext/demo-file-dec.txt
+echo "#"
 
 echo "\n###################################################################################"
 echo "conclusion"
-echo "\###################################################################################"
+echo "###################################################################################"
 echo "\nThe Gramine-SGX run and direct Run with the decrypted file both achieve the same output. \nBut the in the Gramine-SGX run the file is never stored in a decrypted state on the system."
